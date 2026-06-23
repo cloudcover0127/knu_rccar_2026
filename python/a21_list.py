a88a725d5e8b1ef393a9f8e79ab99ee620465d7b
cloudcover0127 <zhxbxbjsjs@gmail.com>
2026-06-23 13:54:00 +0900

    list

---
+++ python/a21_list.py
@@ -0,0 +1,24 @@
+import datetime
+
+
+def main():
+    list_a = []
+    list_b = list()
+    print(type(list_a))
+    print(type(list_b))
+    ptime = datetime.datetime.now()
+    list_c = [1,2,3.3,"jang",ptime,True]
+
+    print(list_c[3])
+    print(list_c[-1])
+    list_c[0] = "un"
+    print(list_c)
+
+    list_d = [[1,2,3],[4,5,6],[7,8,9]]
+    print(list_d[1][2])
+    print(list_d[0][1])
+
+    print(list_d)
+
+if __name__ == "__main__":
+    main()
