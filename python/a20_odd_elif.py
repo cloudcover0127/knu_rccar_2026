6b4724ec6cb114570af680e7b1cfebd87b9108da
cloudcover0127 <zhxbxbjsjs@gmail.com>
2026-06-23 11:43:48 +0900

    1.4

---
+++ python/a20_odd_elif.py
@@ -0,0 +1,12 @@
+def main():
+    number = int(input("정수를 입력하세요>"))
+
+    # if number % 2 == 0:
+    #     print("짝수입니다.")
+    # else:
+    #     print("홀수입니다.")
+    print("홀수" number % 2 else "짝수","입니다.")
+
+
+if __name__ == "__main__":
+    main()
