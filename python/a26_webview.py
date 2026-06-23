cb5c63161118fb168be6849100fa5594598fcbc3
cloudcover0127 <zhxbxbjsjs@gmail.com>
2026-06-23 16:49:42 +0900

    1.13

--- python/a26_webview.py
+++ python/a26_webview.py
@@ -1,6 +1,16 @@
+#pip install pywebview
 import webview
+from pathlib import Path
 
 def main():
-    webview.create_window()
+    webview.create_window(
+        "simple text",
+        url=Path(Path(__file__).resolve() / "text.html").as_uri(),
+        width=640,
+        height=520,
+        resizable=True
+    )
+    webview.start()
 
 if __name__ == "__main__":
+    main()
